d0c2a99fa7e254f2fad19662a11517296b3bd9d5
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2023-06-20 12:21:38 +0300

    Add package argument (#2)
    * pass package as argument
    
    * install peer dependencies of external package
    
    * use save argument
    
    * remove test package-lock
    
    * 1.3.0
    
    ---------
    
    Co-authored-by: Kyriakos Barbounakis <k.barbounakis@gmmail.com>
    Co-authored-by: Kyriakos Barmpounakis <kbarbounakis@it.auth.gr>

--- README.md
+++ README.md
@@ -3,4 +3,14 @@ Install peer dependencies script
 
 ## Usage
 
-npx @themost/peers add-peers
+    npx @themost/peers add-peers
+
+### Install peer dependencies of module
+
+Use `@themost/peers` to install missing peer dependencies of a package which is already a dependency of project. 
+
+    npx @themost/peers <module> [--save|--no-save|--save-optional|--save-dev|--save-exact|--save-peer]
+
+e.g.
+
+    npx @themost/peers @themost/angular --save-dev
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { resolve } = require('path');
 const { spawn } = require('child_process');
 const os = require('os');
+const  minimist = require('minimist');
+const validate = require('validate-npm-package-name');
 
 function working() {
     let P = ['\\', '|', '/', '-'];
@@ -26,7 +28,26 @@ function stopWorking(workingInterval) {
 (function() {
     try {
         const cwd = process.cwd();
-        const package = require(resolve(cwd, './package.json'));
+        const argv = minimist(process.argv.slice(2));
+        // get source package.json
+        let package = require(resolve(cwd, './package.json'));
+        let externalPackage = false;
+        if (argv._.length) {
+            // try to identify the given package.json
+            try {
+                const package0 = argv._[0];
+                const validation = validate(package0);
+                if (validation.errors && validation.errors.length) {
+                    throw new Error(validation.errors[0]);
+                }
+                package = require(resolve(cwd, 'node_modules', package0 , './package.json'));
+                externalPackage = true;
+            } catch (err) {
+                console.error('An error occurred while trying to identify the given package under node_modules dir');
+                console.error(err);
+                return process.exit(-2);
+            }
+        }
         if (Object.prototype.hasOwnProperty.call(package, 'peerDependencies')) {
             let packages = Object.keys(package.peerDependencies).map(function(key) {
                 return `${key}@"${package.peerDependencies[key]}"`
@@ -38,26 +59,34 @@ function stopWorking(workingInterval) {
             const args = [
                 'i'
             ];
+            let saveOption = '--no-save';
+            if (externalPackage === true) {
+                saveOption = '--save';
+                if (argv['save-optional']) {
+                    saveOption = '--save-optional'
+                } else if (argv['save-dev']) {
+                    saveOption = '--save-dev'
+                } else if (argv['save-exact']) {
+                    saveOption = '--save-exact'
+                } else if (argv['save-peer']) {
+                    saveOption = '--save-peer'
+                } else if (argv['save'] === false) {
+                    saveOption = '--no-save'
+                }
+            }
             args.push.apply(args, packages);
             args.push.apply(args, [
-                '--no-save',
+                saveOption,
             ]);
             process.stdout.write(`npm ${args.join(' ')}\n`);
-            const workingInterval = working();
             // change command to npm.cmd on windows
             const cmd = os.type() === 'Windows_NT' ? 'npm.cmd' : 'npm';
             const windowsVerbatimArguments = true;
+            const stdio = 'inherit';
             const npm = spawn(cmd, args, {
                 cwd, // set current directory
-                windowsVerbatimArguments // do not escape arguments (windows only)
-            });
-            npm.stdout.on('data', (data) => {
-                stopWorking(workingInterval);
-                process.stdout.write(data);
-              });
-            npm.stderr.on('data', (data) => {
-                stopWorking(workingInterval);
-                process.stderr.write(data);
+                windowsVerbatimArguments, // do not escape arguments (windows only)
+                stdio // set stdout and stderr to parent
             });
             npm.on('close', (code) => {
                 return process.exit(code);
